94c3ebec3973107cce667080eed070a4e41e5e2f
Ulukele <36236949+Ulukele@users.noreply.github.com>
2019-02-23 23:33:00 +0700

    Added all functions
    Add GetList() and GetAcc()

--- SmartContracts.py
+++ SmartContracts.py
@@ -5,38 +5,11 @@ import sys
 import urllib
 from eth_account import Account
 
+#bytecode
+byte = "608060405234801561001057600080fd5b50610928806100206000396000f3fe608060405234801561001057600080fd5b5060043610610074576000357c0100000000000000000000000000000000000000000000000000000000900480630198489214610079578063a191247214610136578063a5440fa014610140578063c18c749d1461019f578063d3a72ea11461025a575b600080fd5b6100bb6004803603602081101561008f57600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190505050610317565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156100fb5780820151818401526020810190506100e0565b50505050905090810190601f1680156101285780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b61013e6103c7565b005b6101486104dc565b6040518080602001828103825283818151815260200191508051906020019060200280838360005b8381101561018b578082015181840152602081019050610170565b505050509050019250505060405180910390f35b610258600480360360208110156101b557600080fd5b81019080803590602001906401000000008111156101d257600080fd5b8201836020820111156101e457600080fd5b8035906020019184600183028401116401000000008311171561020657600080fd5b91908080601f016020809104026020016040519081016040528093929190818152602001838380828437600081840152601f19601f82011690508083019250505050505050919291929050505061056a565b005b61029c6004803603602081101561027057600080fd5b81019080803573ffffffffffffffffffffffffffffffffffffffff169060200190929190505050610777565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156102dc5780820151818401526020810190506102c1565b50505050905090810190601f1680156103095780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b60006020528060005260406000206000915090508054600181600116156101000203166002900480601f0160208091040260200160405190810160405280929190818152602001828054600181600116156101000203166002900480156103bf5780601f10610394576101008083540402835291602001916103bf565b820191906000526020600020905b8154815290600101906020018083116103a257829003601f168201915b505050505081565b600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002060009054906101000a900460ff16151561041f57600080fd5b6000600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002060006101000a81548160ff02191690831515021790555060206040519081016040528060008152506000803373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002090805190602001906104d9929190610857565b50565b6060600480548060200260200160405190810160405280929190818152602001828054801561056057602002820191906000526020600020905b8160009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019060010190808311610516575b5050505050905090565b600160003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002060009054906101000a900460ff161515156105c357600080fd5b806000803373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000209080519060200190610615929190610857565b5060018060003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002060006101000a81548160ff0219169083151502179055506000600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200190815260200160002054141561077457600360008154809291906001019190505550600354600260003373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000208190555060043390806001815401808255809150509060018203906000526020600020016000909192909190916101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff160217905550505b50565b60606000808373ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff1681526020019081526020016000208054600181600116156101000203166002900480601f01602080910402602001604051908101604052809291908181526020018280546001816001161561010002031660029004801561084b5780601f106108205761010080835404028352916020019161084b565b820191906000526020600020905b81548152906001019060200180831161082e57829003601f168201915b50505050509050919050565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f1061089857805160ff19168380011785556108c6565b828001600101855582156108c6579182015b828111156108c55782518255916020019190600101906108aa565b5b5090506108d391906108d7565b5090565b6108f991905b808211156108f55760008160009055506001016108dd565b5090565b9056fea165627a7a7230582064bd33f7963c0a24c0da2d40773bcd7207abe6709d38625c3fddf058ace23a4c0029"
 
-byte = "608060405234801561001057600080fd5b5061068c806100206000396000f3fe608060405234801561001057600080fd5b5060043610610073577c01000000000000000000000000000000000000000000000000000000006000350463019848928114610078578063a191247214610120578063c18c749d1461012a578063d3a72ea1146101d0578063f6b9ef4c14610203575b600080fd5b6100ab6004803603602081101561008e57600080fd5b503573ffffffffffffffffffffffffffffffffffffffff166102f9565b6040805160208082528351818301528351919283929083019185019080838360005b838110156100e55781810151838201526020016100cd565b50505050905090810190601f1680156101125780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b610128610393565b005b6101286004803603602081101561014057600080fd5b81019060208101813564010000000081111561015b57600080fd5b82018360208201111561016d57600080fd5b8035906020019184600183028401116401000000008311171561018f57600080fd5b91908080601f0160208091040260200160405190810160405280939291908181526020018383808284376000920191909152509295506103ef945050505050565b6100ab600480360360208110156101e657600080fd5b503573ffffffffffffffffffffffffffffffffffffffff1661044b565b6102a96004803603602081101561021957600080fd5b81019060208101813564010000000081111561023457600080fd5b82018360208201111561024657600080fd5b8035906020019184600183028401116401000000008311171561026857600080fd5b91908080601f016020809104026020016040519081016040528093929190818152602001838380828437600092019190915250929550610501945050505050565b60408051602080825283518183015283519192839290830191858101910280838360005b838110156102e55781810151838201526020016102cd565b505050509050019250505060405180910390f35b600060208181529181526040908190208054825160026001831615610100026000190190921691909104601f81018590048502820185019093528281529290919083018282801561038b5780601f106103605761010080835404028352916020019161038b565b820191906000526020600020905b81548152906001019060200180831161036e57829003601f168201915b505050505081565b3360009081526001602052604090205460ff1615156103b157600080fd5b336000818152600160209081526040808320805460ff1916905580518083018083528482529484529183905290912090516103ec92906105c5565b50565b3360009081526001602052604090205460ff161561040c57600080fd5b33600090815260208181526040909120825161042a928401906105c5565b5050336000908152600160208190526040909120805460ff19169091179055565b73ffffffffffffffffffffffffffffffffffffffff81166000908152602081815260409182902080548351601f60026000196101006001861615020190931692909204918201849004840281018401909452808452606093928301828280156104f55780601f106104ca576101008083540402835291602001916104f5565b820191906000526020600020905b8154815290600101906020018083116104d857829003601f168201915b50505050509050919050565b60606002826040518082805190602001908083835b602083106105355780518252601f199092019160209182019101610516565b51815160209384036101000a6000190180199092169116179052920194855250604080519485900382018520805480840287018401909252818652935091508301828280156104f557602002820191906000526020600020905b815473ffffffffffffffffffffffffffffffffffffffff16815260019091019060200180831161058f5750505050509050919050565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f1061060657805160ff1916838001178555610633565b82800160010185558215610633579182015b82811115610633578251825591602001919060010190610618565b5061063f929150610643565b5090565b61065d91905b8082111561063f5760008155600101610649565b9056fea165627a7a72305820f921059aa401501dd52be284799f9cb6d3eecbf1fd06ffb264661fa8263750f00029"
-
+#abi code
 abi = json.loads("""[
-	{
-		"constant": true,
-		"inputs": [
-			{
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "name",
-		"outputs": [
-			{
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [],
-		"name": "RemoveName",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
 	{
 		"constant": false,
 		"inputs": [
@@ -47,6 +20,20 @@ abi = json.loads("""[
 		],
 		"name": "AddName",
 		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "GetAll",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address[]"
+			}
+		],
 		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "function"
@@ -72,21 +59,30 @@ abi = json.loads("""[
 	},
 	{
 		"constant": false,
+		"inputs": [],
+		"name": "RemoveName",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
 		"inputs": [
 			{
-				"name": "_name",
-				"type": "string"
+				"name": "",
+				"type": "address"
 			}
 		],
-		"name": "GetAddresses",
+		"name": "name",
 		"outputs": [
 			{
 				"name": "",
-				"type": "address[]"
+				"type": "string"
 			}
 		],
 		"payable": false,
-		"stateMutability": "nonpayable",
+		"stateMutability": "view",
 		"type": "function"
 	}
 ]""")
@@ -102,6 +98,7 @@ def GetAdres(privateKey):
 
 def SetTransaction(abi, byte, person):
 
+	#create transaction
     contract = web3.eth.contract(abi=abi, bytecode=byte)
     SignedTX = contract.constructor().buildTransaction({
     'from': person.address,
@@ -112,76 +109,138 @@ def SetTransaction(abi, byte, person):
     RawTX = web3.eth.sendRawTransaction(SignedTX.rawTransaction)
     TXReceipt = web3.eth.waitForTransactionReceipt(RawTX)
 
+	#write transaction info
     with open('database.json', 'w') as file:
         file.write(json.dumps({'registrar': TXReceipt['contractAddress'], 'startBlock': TXReceipt['blockNumber']}))
     return {'registrar': TXReceipt['contractAddress'], 'startBlock': TXReceipt['blockNumber']}
 
-def AddName(Caddres, person, name):
-    contract_by_address =  web3.eth.contract(address = Caddres, abi = abi)
-    tx_wo_sign = contract_by_address.functions.AddName(name).buildTransaction({
-        'from': person.address,
-        'nonce': web3.eth.getTransactionCount(person.address),
-        'gas': 8000000,
-        'gasPrice': GetGas()
+def AddName(Caddres, abi, byte, person, name):
+	#create transaction
+	contract_by_address =  web3.eth.contract(address = Caddres, abi = abi)
+	tx_wo_sign = contract_by_address.functions.AddName(name).buildTransaction({
+		'from': person.address,
+		'nonce': web3.eth.getTransactionCount(person.address),
+		'gas': 8000000,
+		'gasPrice': GetGas()
     })
-    signed_tx = person.signTransaction(tx_wo_sign)
-    try:
-        txId = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-    except:
-        return {'status': -1}
+	signed_tx = person.signTransaction(tx_wo_sign)
+	#sending transaction
+	try:
+		txId = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+	except:
+		return {'status': -1}
 
-    #txId = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-    txReceipt = web3.eth.waitForTransactionReceipt(txId)
-    return txReceipt
+	txReceipt = web3.eth.waitForTransactionReceipt(txId)
+	return txReceipt
 
-def RemoveName(person):
-    contract_by_address =  web3.eth.contract(address = Caddres, abi = abi)
-    tx_wo_sign = contract_by_address.functions.RemoveName().buildTransaction({
-        'from': person.address,
-        'gas': 8000000,
-        'nonce': web3.eth.getTransactionCount(person.address),
-        'gasPrice': GetGas()
-    })
-    signed_tx = person.signTransaction(tx_wo_sign)
-    txId = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-    txReceipt = web3.eth.waitForTransactionReceipt(txId)
-    return txReceipt
+def RemoveName(person, abi):
+	#create transaction
+	contract_by_address =  web3.eth.contract(address = Caddres, abi = abi)
+	tx_wo_sign = contract_by_address.functions.RemoveName().buildTransaction({
+    	'from': person.address,
+    	'gas': 8000000,
+    	'nonce': web3.eth.getTransactionCount(person.address),
+    	'gasPrice': GetGas()
+	})
+	signed_tx = person.signTransaction(tx_wo_sign)
+	#sending transaction
+	try:
+		txId = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+	except:
+		return {'status': -1}
 
-def GetName(Caddres, person):
+	txReceipt = web3.eth.waitForTransactionReceipt(txId)
+	return txReceipt
+
+def GetName(Caddres, abi, person):
     contract_by_address = web3.eth.contract(address = Caddres, abi = abi)
     return contract_by_address.functions.GetName(person).call()
 
+def GetList(Caddres, abi):
+	contract_by_address = web3.eth.contract(address = Caddres, abi = abi)
+	RawList = contract_by_address.functions.GetAll().call()
+	list = {}
+	for i in RawList:
+		name = GetName(Caddres, abi, i)
+		if name != '':
+			list[name] = i
+	return list
 
+def GetAcc(Caddres, abi, name):
+	RawList = GetList(Caddres, abi)
+	list = []
+	for i in RawList:
+		if i == name:
+			list.append(RawList[i])
+	return list
+
+
+
+#Connection
 web3 = Web3(HTTPProvider('https://sokol.poa.network'))
 
+#Get account info
 with open('account.json') as file:
     privateKey = json.load(file)['account']
 address = GetAdres(privateKey)
 
+#get arguments
 args = (sys.argv)[1:]
+if(len(args) > 2):
+	for i in range(2, len(args)):
+		args[1] = args[1] + " " + args[i]
 
+
+#Get transaction info
 with open('database.json') as file:
     Caddres = json.load(file)['registrar']
 
 if args[0] == '--deploy':
-    TRInfo = SetTransaction(abi, byte, address)
+	TRInfo = SetTransaction(abi, byte, address)
+	print("Contract address:", TRInfo['registrar'])
     #print(TRInfo)
+
 
 if args[0] == '--add':
-    TRInfo = AddName(Caddres, address, args[1])
-    #print(TRInfo)
-    if(TRInfo['status'] == 0):
-        print("One account must correspond one name")
-    if(TRInfo['status'] == -1):
-        print("No enough funds to add name")
+	TRInfo = AddName(Caddres, abi, byte, address, args[1])
+	if(TRInfo['status'] == 0):
+		print("One account must correspond one name")
+	if(TRInfo['status'] == -1):
+		print("No enough funds to add name")
+	if(TRInfo['status'] == 1):
+		print("Successfully added by", (TRInfo["transactionHash"]).hex())
+	#print(TRInfo)
 
 if args[0] == '--del':
-    TRInfo = RemoveName(address)
-    if TRInfo['status'] == 0:
-        print("No name found for your account")
+	TRInfo = RemoveName(address, abi)
+	if(TRInfo['status'] == 0):
+		print("No name found for your account")
+	if(TRInfo['status'] == -1):
+		print("No enough funds to delete name")
+	if(TRInfo['status'] == 1):
+		print("Successfully deleted by",  TRInfo["transactionHash"].hex())
+	#print(TRInfo)
 
 if args[0] == '--getname':
-    print(GetName(Caddres, web3.toChecksumAddress(args[1])))
+	name = GetName(Caddres, abi, web3.toChecksumAddress(args[1]))
+	if(name == ''):
+		print("No name registered for this account")
+	else:
+		print("Registered account is", name)
+
+if args[0] == '--list':
+	list = GetList(Caddres, abi)
+	for i in list:
+		print(i,': ', list[i], sep ='')
+
+if args[0] == '--getacc':
+	list = GetAcc(Caddres, abi, args[1])
+	if(len(list) == 0):
+		print("No account registered for this name")
+	else:
+		print("Registered accounts are:")
+		for i in list:
+			print(i)
 
 """
 print("\nFile Info:\n")
